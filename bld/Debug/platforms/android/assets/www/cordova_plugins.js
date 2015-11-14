3a8c914dec355ebbb8248bbbbce6914f3f71128b
androiddenemeleri <androiddenemeleri@gmail.com>
2015-11-15 00:23:33 +0200

    poıuytr

--- bld/Debug/platforms/android/assets/www/cordova_plugins.js
+++ bld/Debug/platforms/android/assets/www/cordova_plugins.js
@@ -2,6 +2,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.phonegap.plugin.statusbar": "1.1.0"
+}
 // BOTTOM OF METADATA
 });
